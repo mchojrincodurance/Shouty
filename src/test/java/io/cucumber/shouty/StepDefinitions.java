57ca9231b8389fa28833acc9ebefc4ca61f617b4
Mauro Chojrin <mauro.chojrin@codurance.com>
2022-08-09 16:03:05 +0200

    Step definitions completed

---
+++ src/test/java/io/cucumber/shouty/StepDefinitions.java
@@ -0,0 +1,29 @@
+package io.cucumber.shouty;
+
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
+import static java.util.Arrays.asList;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class StepDefinitions {
+
+    private final Person sean = new Person();
+    private final Person lucy = new Person();
+    private String messageFromSean;
+
+    @Given("Lucy is located {int} metres from Sean")
+    public void lucy_is_located_metres_from_sean(Integer distance) {
+        lucy.moveTo(distance);
+    }
+    @When("Sean shouts {string}")
+    public void sean_shouts(String message) {
+        sean.shout(message);
+        messageFromSean = message;
+    }
+    @Then("Lucy hears Sean's message")
+    public void lucy_hears_sean_s_message() {
+        assertEquals(asList(messageFromSean), lucy.getMessagesHeard());
+    }
+}
